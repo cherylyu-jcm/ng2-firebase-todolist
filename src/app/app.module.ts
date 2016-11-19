253cd1b2fa73ec18086be7071448341f3f2770e5
Cheryl Yu <cherylworkshop@gmail.com>
2016-11-19 17:41:01 +0800

    set config and injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_QDpE-62AlH23NvYGvLTFMzgpge99H3k",
+  authDomain: "ng2-todolist.firebaseapp.com",
+  databaseURL: "https://ng2-todolist.firebaseio.com",
+  storageBucket: "ng2-todolist.appspot.com",
+  messagingSenderId: "991892439957"
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
